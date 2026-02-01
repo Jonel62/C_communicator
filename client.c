94e04a31596977183a997655d93dce3214d11dac
Jonel <lubiestejki47@gmail.com>
2026-02-01 20:54:52 +0100

    user lists, groups, server communicates

--- client.c
+++ client.c
@@ -5,25 +5,78 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <time.h>
+#include <signal.h>
+
 #define MAX 256
+#define NAME_LENGTH 16
+
+//communicates
 #define LOGIN 1
 #define MESSAGE 2
+#define USER_LIST 3
+#define SERVER_COMMUNICATE 4
+#define HEART_BEAT 5
+#define MAKE_GROUP 6
+#define JOIN_GROUP 7
+#define GROUP_MESSAGE 8
+#define GROUPS_LIST 9
+#define GROUP_USERS 10
+#define LEAVE_GROUP 11
 
 struct message {
     long mesg_type;
     int sender_id;
     int receiver_id;
+    char name[NAME_LENGTH];
     char mesg_text[MAX];
 };
+
+int myid;
+
+void handle_sigint(int sig) {
+    printf("\nRecived signal %d. Deleting queue...\n", sig);
+
+    if (msgctl(myid, IPC_RMID, NULL) == -1) {
+        perror("Queue error");
+        exit(1);
+    }
+
+    printf("Queue %d was deleted\nQuitting...\n", myid);
+    exit(0);
+}
+
+void send_message(struct message* msg, int sender_id, int sid) {
+    printf("Which user?\n");
+    scanf("%s", &msg->name);
+    msg->sender_id = sender_id;
+    printf("Send message:");
+    getchar();
+    fgets(msg->mesg_text, MAX, stdin);
+
+    msgsnd(sid, msg,
+       sizeof(*msg) - sizeof(long),
+       0);
+}
+
+void recieve_message(struct message* msg) {
+    msgrcv(myid, msg,
+   sizeof(*msg) - sizeof(long),
+   0, 0);
+    if (msg->mesg_type == MESSAGE) {
+        printf("\nReceived message from user %s:\n %s\n", msg->name, msg->mesg_text);
+    }
+    else if (msg->mesg_type == SERVER_COMMUNICATE) {
+        printf("\nReceived server communicate:\n %s\n", msg->mesg_text);
+    }
+}
 
 int main() {
     srand(time(NULL));
     int id;
     int r = rand()%100000;
-    int myid = msgget(r, 0666 | IPC_CREAT);
-
+    myid = msgget(r, 0666 | IPC_CREAT);
     key_t serverkey = ftok("server", 65);
-    int sid = msgget(serverkey, 0666 | IPC_CREAT);
+    int sid = msgget(serverkey, 0666);
 
     struct message msg;
     msg.mesg_type = LOGIN;
@@ -31,6 +84,8 @@ int main() {
     msg.receiver_id = 0;
     printf("Enter your username: ");
     scanf("%s", msg.mesg_text);
+    char name[NAME_LENGTH];
+    strcpy(name, msg.mesg_text);
 
     msgsnd(sid, &msg,
            sizeof(msg) - sizeof(long),
@@ -42,28 +97,102 @@ int main() {
     printf("Your id is: %d\n", msg.receiver_id);
     id=msg.receiver_id;
     if (fork() == 0) {
+        signal(SIGINT, handle_sigint);
+        signal(SIGTERM, handle_sigint);
+
         printf("From server: %s\n", msg.mesg_text);
         while (1) {
-            printf("What you want to do (message=2, logout): ");
-            scanf("%d", &msg.mesg_type);
+            printf("What do you want to do (message=2, user_list=3, make_group=6): ");
+            scanf("%ld", &msg.mesg_type);
             if (msg.mesg_type == MESSAGE) {
-                printf("Which user?\n");
-                scanf("%d", &msg.receiver_id);
+                send_message(&msg, id, sid);
+            }
+            else if (msg.mesg_type == USER_LIST) {
                 msg.sender_id = id;
-                printf("Send message:");
-                scanf("%s", &msg.mesg_text);
+                msg.receiver_id = -1;
                 msgsnd(sid, &msg,
                    sizeof(msg) - sizeof(long),
                    0);
+                sleep(1);
+            }
+            else if (msg.mesg_type == MAKE_GROUP) {
+                msg.sender_id = id;
+                msg.receiver_id = -1;
+                printf("Enter group name: ");
+                scanf("%s", msg.mesg_text);
+                msgsnd(sid, &msg,
+                   sizeof(msg) - sizeof(long),
+                   0);
+                sleep(1);
+            }
+            else if (msg.mesg_type == GROUPS_LIST) {
+                msg.sender_id = id;
+                msg.receiver_id = -1;
+                msgsnd(sid, &msg,
+                   sizeof(msg) - sizeof(long),
+                   0);
+                sleep(1);
+            }
+            else if (msg.mesg_type == JOIN_GROUP) {
+                msg.sender_id = id;
+                msg.receiver_id = -1;
+                printf("Enter group name: ");
+                scanf("%s", msg.name);
+                msgsnd(sid, &msg,
+                   sizeof(msg) - sizeof(long),
+                   0);
+            }
+            else if (msg.mesg_type == GROUP_MESSAGE) {
+                msg.sender_id = id;
+                msg.receiver_id = -1;
+                printf("Enter group name: ");
+                scanf("%s", msg.name);
+                printf("Enter message: ");
+                getchar();
+                fgets(msg.mesg_text, MAX, stdin);
+                msgsnd(sid, &msg,
+                   sizeof(msg) - sizeof(long),
+                   0);
+            }
+            else if (msg.mesg_type == GROUP_USERS) {
+                msg.sender_id = id;
+                msg.receiver_id = -1;
+                printf("Enter group name: ");
+                scanf("%s", msg.name);
+                msgsnd(sid, &msg,
+               sizeof(msg) - sizeof(long),
+                0);
+                sleep(1);
+            }
+            else if (msg.mesg_type == LEAVE_GROUP) {
+                msg.sender_id = id;
+                msg.receiver_id = -1;
+                printf("Enter group name: ");
+                scanf("%s", msg.name);
+                msgsnd(sid, &msg,
+                sizeof(msg) - sizeof(long),
+                0);
+                sleep(1);
             }
         }
     }
     else {
         while (1) {
-            msgrcv(myid, &msg,
-           sizeof(msg) - sizeof(long),
-           MESSAGE, 0);
-            printf("\nReceived message from user %d:\n %s\n", msg.sender_id, msg.mesg_text);
+            if (fork() == 0) {
+                recieve_message(&msg);
+            }
+            else {
+                while (1) {
+                    sleep(5);
+                    msg.sender_id = id;
+                    msg.receiver_id = -1;
+                    strcpy(msg.mesg_text, "still alive");
+                    msg.mesg_type = HEART_BEAT;
+                    msgsnd(sid, &msg,
+                       sizeof(msg) - sizeof(long),
+                       0);
+                }
+            }
         }
 
     }
--- server.c
+++ server.c
@@ -1,36 +1,229 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <sys/ipc.h>
 #include <sys/msg.h>
 #include <string.h>
+#include <unistd.h>
+#include <signal.h>
+#include <time.h>
 
+#define NAME_LENGTH 16
 #define MAX 256
+#define MAX_USERS 24
+#define MAX_GROUPS 16
+
+//communicates
 #define LOGIN 1
 #define MESSAGE 2
+#define USER_LIST 3
+#define SERVER_COMMUNICATE 4
+#define HEART_BEAT 5
+#define MAKE_GROUP 6
+#define JOIN_GROUP 7
+#define GROUP_MESSAGE 8
+#define GROUPS_LIST 9
+#define GROUP_USERS 10
+#define LEAVE_GROUP 11
 
 struct user {
     int user_id;
     int queue_id;
-    char name[16];
+    char name[NAME_LENGTH];
+    long last_seen;
 };
 
 struct message {
     long mesg_type;
     int sender_id;
     int receiver_id;
+    char name[NAME_LENGTH];
     char mesg_text[MAX];
 };
 
-int main() {
-    struct user users[10];
-    key_t key = ftok("server", 65);
-    int sid = msgget(key, 0666 | IPC_CREAT);
+const struct user user_default = {-1,-1,""};
 
+void init_user_list(struct user* users) {
+    for (int i=0; i<MAX_USERS; i++) {
+        users[i] = user_default;
+    }
+}
+
+struct group {
+    char name[NAME_LENGTH];
+    int users_id[MAX_USERS];
+};
+
+void send_communicate(char text[MAX], struct message* msg, struct user* user) {
+    msg->receiver_id=user->user_id;
+    msg->sender_id=-1;
+    msg->mesg_type = SERVER_COMMUNICATE;
+    strcpy(msg->mesg_text, text);
+    if (msgsnd(user->queue_id, msg,
+               sizeof(*msg) - sizeof(long),
+               0) == -1) {
+        perror("msgsnd");
+               }
+    printf("Sended communicate to %s\n", user->name);
+}
+
+void init_group_list(struct group* groups) {
+    for (int i=0; i<MAX_USERS; i++) {
+        strcpy(groups[i].name, "");
+        for (int j=0; j<MAX_USERS; j++) {
+            groups[i].users_id[j] = -1;
+        }
+    }
+}
+
+int sid;
+
+void handle_sigint(int sig) {
+    printf("\nRecived signal %d. Deleting queue...\n", sig);
+
+    if (msgctl(sid, IPC_RMID, NULL) == -1) {
+        perror("Queue error");
+        exit(1);
+    }
+
+    printf("Queue %d was deleted\nQuitting...\n", sid);
+    exit(0);
+}
+
+void login_user(struct message* msg, struct user* user, int* i) {
+    user->user_id = *i;
+    user->queue_id = msg->sender_id;
+    user->last_seen = time(NULL);
+    strcpy(user->name, msg->mesg_text);
+    printf("Login from client queue: %d as %s\n", user->queue_id, user->name);
+    send_communicate("logged", msg, user);
+}
+
+int find_free_slot(struct user* users) {
+    for (int j=0; j<MAX_USERS; j++) {
+        if (users[j].user_id < 0) {
+            return j;
+        }
+    }
+    printf("There are none slots avialable\n");
+}
+
+int find_free_group_slot(struct group* groups) {
+    for (int j=0; j<MAX_GROUPS; j++) {
+        if (strcmp(groups[j].name, "") == 0) {
+            return j;
+        }
+    }
+    printf("There are none slots avialable\n");
+}
+
+
+void send_message(struct message* msg, struct user* user) {
+    printf("Message from client: %d to %d\n", msg->sender_id, msg->receiver_id);
+    msg->mesg_type=MESSAGE;
+    if (msgsnd(user->queue_id, msg,
+               sizeof(*msg) - sizeof(long),
+               0) == -1) {
+        perror("msgsnd");
+               }
+    printf("Sended message to queue %d\n", user->queue_id);
+}
+
+void find_receiver(struct message* msg, struct user* users) {
+    for (int j = 0; j < MAX_USERS; j++) {
+        if (strcmp(users[j].name, msg->name) == 0) {
+            msg->receiver_id=users[j].user_id;
+            break;
+        }
+    }
+}
+
+int find_group(struct message* msg, struct group* groups) {
+    for (int j = 0; j < MAX_GROUPS; j++) {
+        if (strcmp(groups[j].name, msg->name) == 0) {
+            return j;
+        }
+    }
+    printf("This group doesnt exist\n");
+}
+
+void send_group_users(struct group* group, struct user* users, struct message* msg) {
+    char text[MAX]="Users in group: ";
+    for (int k=0; k<MAX_USERS; k++) {
+        if (group->users_id[k] >= 0) {
+            strcat(text, users[group->users_id[k]].name);
+            strcat(text, ", ");
+        }
+    }
+    send_communicate(text, msg, &users[msg->sender_id]);
+    printf("Sended users in group list to %s\n", users[msg->receiver_id].name);
+}
+
+void find_sender_name(struct message* msg, struct user* users) {
+    for (int j = 0; j < MAX_USERS; j++) {
+        if (users[j].user_id == msg->sender_id) {
+            strcpy(msg->name, users[j].name);
+            break;
+        }
+    }
+}
+
+void send_groups_list(struct message* msg, struct group* groups, struct user* users) {
+    char text[MAX] = "Currently exisiting groups: ";
+    for (int k=0; k<MAX_USERS; k++) {
+        if (strcmp(groups[k].name, "") != 0) {
+            strcat(text, groups[k].name);
+            strcat(text, ", ");
+        }
+    }
+    send_communicate(text, msg, &users[msg->sender_id]);
+    printf("Sended groups list to %s\n", users[msg->receiver_id].name);
+}
+
+void send_user_list(struct message* msg, struct user* users) {
+    char text[MAX]="Currently logged users: ";
+    for (int k=0; k<MAX_USERS; k++) {
+        if (users[k].user_id >= 0) {
+            strcat(text, users[k].name);
+            strcat(text, ", ");
+        }
+    }
+    send_communicate(text, msg, &users[msg->sender_id]);
+    printf("Sended user list to %s\n", users[msg->receiver_id].name);
+}
+void check_users(struct user* users) {
+    for (int j = 0; j < MAX_USERS; j++) {
+        if (users[j].user_id < 0) {
+            continue;
+        }
+        long last_seen = time(NULL) - users[j].last_seen;
+        if (last_seen >= 10) {
+            users[j]=user_default;
+            printf("User %d not responding\n deleting user...\n", users[j].user_id);
+        }
+    }
+}
+
+void make_group(int* i, struct group* group, struct message* msg) {
+    strcpy(group->name, msg->mesg_text);
+    group->users_id[0]=msg->sender_id;
+    printf("New group %s\n", group->name);
+}
+
+int main() {
+    struct group groups[MAX_GROUPS];
+    struct user users[MAX_USERS];
+    init_user_list(users);
+    init_group_list(groups);
+    key_t key = ftok("server", 65);
+    sid = msgget(key, 0666 | IPC_CREAT);
+    signal(SIGINT, handle_sigint);
+    signal(SIGTERM, handle_sigint);
     struct message msg;
 
     printf("Server started...\n");
     int i = 0;
     while (1) {
-
+        check_users(users);
         if (msgrcv(sid, &msg,
                    sizeof(msg) - sizeof(long),
                    0, 0) == -1) {
@@ -38,29 +231,60 @@ int main() {
             continue;
                    }
         if (msg.mesg_type == LOGIN) {
-            users[i].user_id = i;
-            users[i].queue_id = msg.sender_id;
-            strcpy(users[i].name, msg.mesg_text);
-            printf("Login from client queue: %d as %s\n", users[i].queue_id, users[i].name);
-            msg.mesg_type = MESSAGE;
-            msg.sender_id = 0;
-            msg.receiver_id = users[i].user_id;
-            strcpy(msg.mesg_text, "logged");
-            if (msgsnd(users[i].queue_id, &msg,
-                       sizeof(msg) - sizeof(long),
-                       0) == -1) {
-                perror("msgsnd");
-                       }
-            i++;
+            i = find_free_slot(users);
+            login_user(&msg, &users[i], &i);
         }
         else if (msg.mesg_type == MESSAGE) {
-            printf("Message from client: %d to %d\n", msg.sender_id, msg.receiver_id);
-            if (msgsnd(users[msg.receiver_id].queue_id, &msg,
-                       sizeof(msg) - sizeof(long),
-                       0) == -1) {
-                perror("msgsnd");
-                       }
-            printf("Sended msg to queue %d\n", users[msg.receiver_id].queue_id);
+            send_communicate("Your message has been accepted", &msg, &users[msg.sender_id]);
+            find_receiver(&msg, users);
+            find_sender_name(&msg, users);
+            send_message(&msg, &users[msg.receiver_id]);
+            send_communicate("Your message has been delivered", &msg, &users[msg.sender_id]);
+        }
+        else if (msg.mesg_type == USER_LIST) {
+            send_user_list(&msg, users);
+        }
+        else if (msg.mesg_type == HEART_BEAT) {
+            users[msg.sender_id].last_seen = time(NULL);
+        }
+        else if (msg.mesg_type == MAKE_GROUP) {
+            i=find_free_group_slot(groups);
+            make_group(&i, &groups[i], &msg);
+        }
+        else if (msg.mesg_type == GROUPS_LIST) {
+            send_groups_list(&msg, groups, &users);
+        }
+        else if (msg.mesg_type == JOIN_GROUP) {
+            i=find_group(&msg, groups);
+            groups[i].users_id[msg.sender_id]=users[msg.sender_id].user_id;
+            printf("Added user %d to group %s\n", msg.sender_id, groups[i].name);
+        }
+        else if (msg.mesg_type == GROUP_MESSAGE) {
+            printf("Message to group %s from %d\n", msg.name, msg.sender_id);
+            send_communicate("Your message has been accepted", &msg, &users[msg.sender_id]);
+            i=find_group(&msg, groups);
+            find_sender_name(&msg, users);
+            for (int k=0; k<MAX_USERS; k++) {
+                if (groups[i].users_id[k] < 0 || groups[i].users_id[k] == msg.sender_id) {
+                    continue;
+                }
+                msg.receiver_id=groups[i].users_id[k];
+                send_message(&msg, &users[groups[i].users_id[k]]);
+            }
+            send_communicate("Your message has been delivered to group", &msg, &users[msg.sender_id]);
+        }
+        else if (msg.mesg_type == GROUP_USERS) {
+            i=find_group(&msg, groups);
+            send_group_users(&groups[i], users, &msg);
+            }
+        else if (msg.mesg_type == LEAVE_GROUP) {
+            i=find_group(&msg, groups);
+            for (int k=0; k<MAX_USERS; k++) {
+                if (groups[i].users_id[k] == msg.sender_id) {
+                    groups[i].users_id[k] = -1;
+                    }
+                }
+            send_communicate("You've been removed from the group.\n", &msg, &users[msg.sender_id]);
+            }
         }
     }
-}
